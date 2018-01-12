a0938ab64e6515d02f962977db864939af8d8ce7
prawadhwani <ieyesight@live.com>
2018-01-12 10:16:13 +0000

    test again

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def hello():
-    return "Hello world with cicd!"
+    return "bsdbfsdbf Hello world with cicd!"
 
 if __name__ == "__main__":
     # Only for debugging while developing
