aa37fe1e5b72f16bee56428c702bc2c51ffbf71c
arcticf0x <ieyesight@live.com>
2018-01-13 11:16:39 +0530

    test ci

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def hello():
-    return "this has to work"
+    return "this is another test"
 
 if __name__ == "__main__":
     # Only for debugging while developing
