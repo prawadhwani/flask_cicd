5c9ad69bdc38edfe04f31530181b090e6c29f882
arcticf0x <ieyesight@live.com>
2018-02-04 00:47:41 +0530

    ipv6 testing

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -10,4 +10,4 @@ def hello():
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=8080)
