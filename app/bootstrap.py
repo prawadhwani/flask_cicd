c3770c7140cdfec00d2b049cea57d269cbde8cac
arcticf0x <ieyesight@live.com>
2018-07-22 22:47:18 +0530

    swarm cicd

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def hello():
-    return " a new test using ingress for the load balancer testing docker swarm"
+    return "boogyman was here a new test using ingress for the load balancer testing docker swarm"
 
 if __name__ == "__main__":
     # Only for debugging while developing
